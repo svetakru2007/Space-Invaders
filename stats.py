d12cd0b70d8cc012ee92c9dd68542a1600f2d526
svetakru <svetakru2007@gmail.ru>
2026-04-25 21:36:46 +0300

    добавлени новой статистики

--- stats.py
+++ stats.py
@@ -4,7 +4,7 @@ class Stats():
     def __init__(self):
         """инициализирует статистику"""
         self.reset_stats()
-        self.run_game = True # Флаг активности игры
+        self.run_game = True # флаг активности игры
 
         # Загрузка рекорда из файла
         with open('high_score.txt', 'r') as f:
@@ -12,5 +12,9 @@ class Stats():
 
     def reset_stats(self):
         """статистика, изменяющаяся во время игры"""
-        self.guns_left = 3 # Кол-во жизней
-        self.score = 0 # Счет
+        self.guns_left = 3 # кол-во жизней
+        self.score = 0 # счет
+        self.level = 1  # текущий уровень
+        self.alien_base_speed = 0.03  # начальная скорость пришельца
+        self.alien_current_speed = 0.03  # текущая скорость пришельца
+        self.current_shape = 'rectangle' # форма армии по умолчанию
